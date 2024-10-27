abe4cbf31b8292c8ab137cbed2840f7c3be828bb
Mohammed N <mnaser11218@gmail.com>
2024-10-27 11:44:27 -0400

    completed classes, need to complete methods in classroom enum

---
+++ src/main/java/com/github/curriculeon/Instructors.java
@@ -0,0 +1,11 @@
+package com.github.curriculeon;
+
+public class Instructors extends People{
+
+    public Instructors() {
+    }
+    public Instructors getInstance(){
+        return this;
+    }
+
+}
