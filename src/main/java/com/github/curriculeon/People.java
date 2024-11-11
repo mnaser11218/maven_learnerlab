c0998700da3303f2741c08de4a604404e6d615d8
Mohammed N <mnaser11218@gmail.com>
2024-11-11 10:11:51 -0500

    updated people class

--- src/main/java/com/github/curriculeon/People.java
+++ src/main/java/com/github/curriculeon/People.java
@@ -1,13 +1,55 @@
 package com.github.curriculeon;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
-public class People<Person> extends ArrayList<Person> {
+public class People implements  Iterable<Person>{
+    private List<Person> personList = new ArrayList<>();
     public People() {
     }
+    public void add(Person person){
+        personList.add(person);
+    }
+    public Person findById(Long id){
+        for(Person person : personList){
+            if(person.getId().equals(id)){
+                return person;
+            }
+        }
+        return null;
+    }
 
-    public People(Collection<? extends Person> c) {
-        super(c);
+    public boolean contains(Person person){
+        boolean results = false;
+        for(Person ele: personList){
+            if(ele.equals(person)){
+                results = true;
+                break;
+            }
+        }
+
+        return results;
+    }
+    public void remove(Person person){
+  personList.remove(person);
+    }
+
+    public void remove(Long id){
+     personList.remove(findById(id));
+    }
+
+    public void removeAll(){
+        this.personList.clear();
+    }
+    public Integer count(){
+       return this.personList.size();
+    }
+    public Person[] toArray(){
+        return this.personList.toArray(new Person[0]);
+    }
+
+
+    @Override
+    public Iterator<Person> iterator() {
+        return personList.iterator();
     }
 }
