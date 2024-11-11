3e718553f0066132c548604333c08936b013002c
Mohammed N <mnaser11218@gmail.com>
2024-11-11 11:31:25 -0500

    added students class instance field

--- src/main/java/com/github/curriculeon/Students.java
+++ src/main/java/com/github/curriculeon/Students.java
@@ -1,10 +1,15 @@
 package com.github.curriculeon;
 
-public class Students extends People{
-    public Students() {
+public final class Students extends People{
+    public static Students INSTANCE = new Students();
+    private Students() {
+        INSTANCE.add(new Student(5l, "mohammed"));
+        INSTANCE.add(new Student(9l, "naser"));
+        INSTANCE.add(new Student(4l, "ayad"));
+
     }
 
-  public Students getInstance(){
-        return this;
+  public static Students getInstance(){
+        return INSTANCE;
   }
 }
