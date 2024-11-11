d2f5601887027704254a02996f42b5d3228502b4
Mohammed N <mnaser11218@gmail.com>
2024-11-10 21:46:55 -0500

    updated teacher interface

--- src/main/java/com/github/curriculeon/Teacher.java
+++ src/main/java/com/github/curriculeon/Teacher.java
@@ -1,6 +1,6 @@
 package com.github.curriculeon;
 
 public interface Teacher {
-    void lecture (Learner learners [], Double time);
-    void teach(Learner learner, Double time);
+    void lecture (Learner [] learners, Double numberOfHours);
+    void teach(Learner learner, Double numberOfHours);
 }
